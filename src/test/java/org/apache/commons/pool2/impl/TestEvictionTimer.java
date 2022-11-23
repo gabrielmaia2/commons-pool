5b6aa89287e7f804a559cc83ba64dca53892a18a
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-11-22 23:20:40 -0300

    fix(tests): Fix test smell magic number test

--- src/test/java/org/apache/commons/pool2/impl/TestEvictionTimer.java
+++ src/test/java/org/apache/commons/pool2/impl/TestEvictionTimer.java
@@ -66,8 +66,12 @@ public class TestEvictionTimer {
             final Field evictorExecutorField = EvictionTimer.class.getDeclaredField("executor");
             evictorExecutorField.setAccessible(true);
             final ThreadPoolExecutor evictionExecutor = (ThreadPoolExecutor) evictorExecutorField.get(null);
-            assertEquals(2, evictionExecutor.getQueue().size()); // Reaper plus one eviction task
-            assertEquals(1, EvictionTimer.getNumTasks());
+
+            int expectedQueueSize = 2;
+            int expectedNumberTasks = 1;
+
+            assertEquals(expectedQueueSize, evictionExecutor.getQueue().size()); // Reaper plus one eviction task
+            assertEquals(expectedNumberTasks, EvictionTimer.getNumTasks());
 
             // Start evictor #2
             final BaseGenericObjectPool<String, RuntimeException>.Evictor evictor2 = pool.new Evictor();
@@ -78,8 +82,12 @@ public class TestEvictionTimer {
             sf = (ScheduledFuture<?>) evictorTaskFutureField.get(evictor2);
             assertFalse(sf.isCancelled());
             // 2- and, the eviction action is added to executor thread pool
-            assertEquals(3, evictionExecutor.getQueue().size()); // Reaper plus 2 eviction tasks
-            assertEquals(2, EvictionTimer.getNumTasks());
+
+            expectedQueueSize = 3;
+            expectedNumberTasks = 2;
+
+            assertEquals(expectedQueueSize, evictionExecutor.getQueue().size()); // Reaper plus 2 eviction tasks
+            assertEquals(expectedNumberTasks, EvictionTimer.getNumTasks());
 
             // Stop evictor #1
             EvictionTimer.cancel(evictor1, BaseObjectPoolConfig.DEFAULT_EVICTOR_SHUTDOWN_TIMEOUT, false);
@@ -90,8 +98,12 @@ public class TestEvictionTimer {
             assertTrue(sf.isCancelled());
             // 2- and, the eviction action is removed from executor thread pool
             final ThreadPoolExecutor evictionExecutorOnStop = (ThreadPoolExecutor) evictorExecutorField.get(null);
-            assertEquals(2, evictionExecutorOnStop.getQueue().size());
-            assertEquals(1, EvictionTimer.getNumTasks());
+
+            expectedQueueSize = 2;
+            expectedNumberTasks = 1;
+
+            assertEquals(expectedQueueSize, evictionExecutorOnStop.getQueue().size());
+            assertEquals(expectedNumberTasks, EvictionTimer.getNumTasks());
 
             // Stop evictor #2
             EvictionTimer.cancel(evictor2, BaseObjectPoolConfig.DEFAULT_EVICTOR_SHUTDOWN_TIMEOUT, false);
@@ -102,7 +114,10 @@ public class TestEvictionTimer {
             assertTrue(sf.isCancelled());
             // 2- and, the eviction thread pool executor is freed
             assertNull(evictorExecutorField.get(null));
-            assertEquals(0, EvictionTimer.getNumTasks());
+
+            expectedNumberTasks = 0;
+
+            assertEquals(expectedNumberTasks, EvictionTimer.getNumTasks());
         }
     }
 }
